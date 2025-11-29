6ea87fc2b54239a3e012339dafd3e7a783d14a44
mazen ahmed mohamed abd elhamed <135850749+mazenshama@users.noreply.github.com>
2025-11-29 05:20:36 +0200

    Updates

---
+++ browser/chunk-DVCEIMDV.js
@@ -0,0 +1,1 @@
+var t={production:!1,apiBaseUrl:"http://publictransporttraker.runasp.net/api"};export{t as a};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'http://publictransporttraker.runasp.net/api'
-};
